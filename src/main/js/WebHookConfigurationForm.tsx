a849f4f95103be12b6477a03ba4d751c80e63123
Florian Scholdei <florian.scholdei@cloudogu.com>
2022-02-16 21:24:54 +0100

    Correct delete icon in form, use fc

--- src/main/js/WebHookConfigurationForm.tsx
+++ src/main/js/WebHookConfigurationForm.tsx
@@ -21,140 +21,112 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import React from "react";
-import { WithTranslation, withTranslation } from "react-i18next";
+import React, { FC, useState } from "react";
+import { useTranslation } from "react-i18next";
+
 import styled from "styled-components";
-import { confirmAlert, DropDown, Help, InputField, Checkbox } from "@scm-manager/ui-components";
+import { Checkbox, ConfirmAlert, Select, Help, Icon, InputField } from "@scm-manager/ui-components";
 import { WebHookConfiguration } from "./WebHookConfiguration";
 
-const DeleteIcon = styled.a`
+const DeleteIcon = styled(Icon)`
   margin: 0.55rem 0 0 0.75rem;
 `;
 
-const DropDownWrapper = styled.div`
-  margin-right: 1.5rem;
-`;
-
-type Props = WithTranslation & {
+type Props = {
   webHook: WebHookConfiguration;
   readOnly: boolean;
   onChange: (p: WebHookConfiguration) => void;
   onDelete: (p: WebHookConfiguration) => void;
 };
 
-type State = WebHookConfiguration;
+const WebHookConfigurationForm: FC<Props> = ({ webHook, readOnly, onChange, onDelete }) => {
+  const [t] = useTranslation("plugins");
+  const [showConfirmAlert, setShowConfirmAlert] = useState(false);
 
-class WebHookConfigurationForm extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = props.webHook;
-  }
-
-  componentWillReceiveProps(nextProps) {
-    // update the webhook in the state if the prop are changed
-    // The prop can be modified if webhooks are deleted
-    if (nextProps.webHook !== this.props.webHook) {
-      this.state = nextProps.webHook;
-    }
-  }
-
-  handleChange = (value: any, name: string) => {
-    this.setState(
-      {
-        [name]: value
-      },
-      () => this.props.onChange(this.state)
-    );
+  const handleChange = (value: string | boolean, name: string) => {
+    onChange({
+      ...webHook,
+      [name]: value
+    });
   };
 
-  renderHttpMethodDropDown = () => {
-    const { readOnly } = this.props;
-    return (
-      <div className="field control">
-        <DropDown
-          options={["GET", "POST", "AUTO", "PUT"]}
-          optionSelected={this.handleDropDownChange}
-          preselectedOption={this.state.method}
-          disabled={readOnly}
+  const options = ["GET", "POST", "AUTO", "PUT"];
+  return (
+    <>
+      {showConfirmAlert ? (
+        <ConfirmAlert
+          title={t("scm-webhook-plugin.confirm-delete.title")}
+          message={t("scm-webhook-plugin.confirm-delete.message")}
+          buttons={[
+            {
+              className: "is-outlined",
+              label: t("scm-webhook-plugin.confirm-delete.submit"),
+              onClick: () => onDelete(webHook)
+            },
+            {
+              label: t("scm-webhook-plugin.confirm-delete.cancel"),
+              onClick: () => null,
+              autofocus: true
+            }
+          ]}
+          close={() => setShowConfirmAlert(false)}
         />
-        <Help message={this.props.t("scm-webhook-plugin.form.methodHelp")} />
-      </div>
-    );
-  };
-
-  handleDropDownChange = (selection: string) => {
-    this.setState({
-      ...this.state,
-      method: selection
-    });
-    this.handleChange(selection, "method");
-  };
-
-  confirmDelete = () => {
-    const { t } = this.props;
-    confirmAlert({
-      title: t("scm-webhook-plugin.confirm-delete.title"),
-      message: t("scm-webhook-plugin.confirm-delete.message"),
-      buttons: [
-        {
-          label: t("scm-webhook-plugin.confirm-delete.submit"),
-          onClick: () => this.props.onDelete(this.state)
-        },
-        {
-          label: t("scm-webhook-plugin.confirm-delete.cancel"),
-          onClick: () => null
-        }
-      ]
-    });
-  };
-
-  render() {
-    const { readOnly, t } = this.props;
-    const { urlPattern, executeOnEveryCommit, sendCommitData } = this.state;
-    const deleteIcon = readOnly ? (
-      ""
-    ) : (
-      <DeleteIcon className="level-item" onClick={this.confirmDelete}>
-        <span className="icon is-small">
-          <i className="fas fa-trash" />
-        </span>
-      </DeleteIcon>
-    );
-    return (
+      ) : null}
       <article className="media">
-        <DropDownWrapper className="media-left">{this.renderHttpMethodDropDown()}</DropDownWrapper>
+        <div className="media-left mr-5">
+          <div className="field control">
+            <Select
+              name="method"
+              value={webHook.method}
+              onChange={handleChange}
+              options={options.map(v => ({ label: v, value: v }))}
+              disabled={readOnly}
+            />
+            <Help message={t("scm-webhook-plugin.form.methodHelp")} />
+          </div>
+        </div>
         <div className="media-content">
           <InputField
             name="urlPattern"
+            value={webHook.urlPattern}
+            onChange={handleChange}
             placeholder={t("scm-webhook-plugin.form.urlPattern")}
-            value={urlPattern}
-            onChange={this.handleChange}
             disabled={readOnly}
           />
           <Checkbox
             name="executeOnEveryCommit"
+            checked={webHook.executeOnEveryCommit}
+            onChange={handleChange}
             label={t("scm-webhook-plugin.form.executeOnEveryCommit")}
-            checked={executeOnEveryCommit}
-            onChange={this.handleChange}
-            disabled={readOnly}
             helpText={t("scm-webhook-plugin.form.executeOnEveryCommitHelp")}
+            disabled={readOnly}
           />
           <Checkbox
             name="sendCommitData"
+            checked={webHook.sendCommitData}
+            onChange={handleChange}
             label={t("scm-webhook-plugin.form.sendCommitData")}
-            checked={sendCommitData}
-            onChange={this.handleChange}
-            disabled={readOnly}
             helpText={t("scm-webhook-plugin.form.sendCommitDataHelp")}
+            disabled={readOnly}
           />
         </div>
         <div>
           <Help message={t("scm-webhook-plugin.form.urlPatternHelp")} />
         </div>
-        <div className="media-right">{deleteIcon}</div>
+        <div className="media-right">
+          {!readOnly && (
+            <DeleteIcon
+              className="level-item"
+              name="trash"
+              title={t("scm-webhook-plugin.form.delete")}
+              color="inherit"
+              onClick={() => setShowConfirmAlert(true)}
+            />
+          )}
+        </div>
       </article>
-    );
-  }
-}
+    </>
+  );
+};
 
-export default withTranslation("plugins")(WebHookConfigurationForm);
+export default WebHookConfigurationForm;
